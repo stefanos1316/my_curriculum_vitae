e1169231a1c83cf03dcba3ba0945ffcf1f8936cc
stefanos1316 <stefanos1316@gmail.com>
2020-04-21 12:37:04 +0300

    Set up deploy check mechanisms

--- start.sh
+++ start.sh
@@ -8,6 +8,26 @@ INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 _FORCE_OPTION=''
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
+function sanitize {
+
+  mv certificates/* web/
+  mv degress/* web/
+  mv github_activity_overview web/
+  mv proofs/* web/
+  mv publications/* web/
+  
+  rm -rf tools
+  rm -rf styles
+  rm Makefile
+  rm -rf markdown
+  rm -rf README.md
+  rm -rf resume.tuc
+  
+  shopt -s extglob
+  rm -rf !(web)
+  mv web/* ./ && rm -rf web/
+}
+
 echo "Push to branch $INPUT_BRANCH";
 [ -z "${INPUT_GITHUB_TOKEN}" ] && {
     echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
@@ -34,22 +54,13 @@ git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TA
 git checkout -b gh-pages
 git pull ${remote_repo} gh-pages
 
-ls -l
-mv certificates/* web/
-mv degress/* web/
-mv github_activity_overview web/
-mv proofs/* web/
-mv publications/* web/
-rm -rf tools
-rm -rf styles
-rm Makefile
-rm -rf markdown
-rm -rf README.md
-rm -rf resume.tuc
-shopt -s extglob
-rm -rf !(web)
-mv web/* ./ && rm -rf web/
+sanitize
 
 git add .
 git commit -m "New deploy - $(date)"
 git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
+
+echo "$GITHUB_ACTOR $GITHUB_EMAIL"
+
+curl https://stefanos1316.github.io/my_curriculum_vitae/ || echo Failed to load
+curl https://stefanos1316.github.io/my_curriculum_vitae/index.html || echo Failed to load
