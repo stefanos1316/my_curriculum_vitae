d98e3e39c7d208e058ea468393cd3f3bee6d465e
stefanos1316 <stefanos1316@gmail.com>
2020-09-10 19:24:31 +0200

    Fix typo

--- start.sh
+++ start.sh
@@ -40,7 +40,7 @@ mv github_activity_overview web/
 mv proofs/* web/
 mv publications/*/* web/
 mv achievements/* web/
-my thesis_reports/* web/
+mv thesis_reports/* web/
 
 rm -rf tools
 rm -rf styles
